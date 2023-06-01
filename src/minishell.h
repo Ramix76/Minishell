2113df23432133894510f77fabb73adfb8a77277
puyma <mpuig-ma@student.42barcelona.com>
2023-06-01 16:44:28 +0200

    renamed files

--- src/minishell.h
+++ src/minishell.h
@@ -6,12 +6,27 @@
 /*   By: framos-p <framos-p@student.42barcel>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/31 11:02:07 by framos-p          #+#    #+#             */
-/*   Updated: 2023/05/31 11:03:31 by framos-p         ###   ########.fr       */
+/*   Updated: 2023/06/01 16:36:08 by mpuig-ma         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef MINISHELL_H
 # define MINISHELL_H
+
+
+#include "builtin.h"
+
+#include <sys/errno.h>
+#include <stdio.h>
+#include <unistd.h>
+#include <string.h>
+#include <stdlib.h>
+#include <limits.h>
+
+#include <readline/readline.h>
+#include <readline/history.h>
+
+#include "libft.h"
 
 typedef struct s_cmd
 {
