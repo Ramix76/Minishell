d1acf304707bf89b30d6672cc94d065156317fc8
puyma <mpuig-ma@student.42barcelona.com>
2023-06-05 17:14:36 +0200

    norm

--- tests/test_ft_getenv.c
+++ tests/test_ft_getenv.c
@@ -6,7 +6,7 @@
 /*   By: mpuig-ma <mpuig-ma@student.42barcel>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/02 16:43:29 by mpuig-ma          #+#    #+#             */
-/*   Updated: 2023/06/02 18:32:45 by mpuig-ma         ###   ########.fr       */
+/*   Updated: 2023/06/05 17:14:23 by mpuig-ma         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,7 +16,7 @@
 
 int	main(int argc, char **argv, char **envp)
 {
-	init_log(argc, argv, envp);	
+	init_log(argc, argv, envp);
 	printf("with envp: %s\n", ft_getenv("PATH", (const char **) envp));
 	return (0);
 }
