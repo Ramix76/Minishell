44ec4f16763f79d9edf55f7d6ddff4b6e430a577
puyma <mpuig-ma@student.42barcelona.com>
2023-06-05 13:13:59 +0200

    void, SS

--- src/builtins/builtins.h
+++ src/builtins/builtins.h
@@ -6,24 +6,22 @@
 /*   By: framos-p <framos-p@student.42barcel>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/02 14:30:49 by framos-p          #+#    #+#             */
-/*   Updated: 2023/06/02 14:40:03 by framos-p         ###   ########.fr       */
+/*   Updated: 2023/06/05 13:08:19 by mpuig-ma         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef BUILTIN_H
-# define BUILTIN_H
+#ifndef BUILTINS_H
+# define BUILTINS_H
 
-#include "minishell.h"
+# include "minishell.h"
 
 typedef struct s_cmd
 {
-	char	*command;
-	char	**tokens;
+	char		*command;
+	char		**tokens;
 }				t_cmd;
 
-int		ft_pwd();
+int		ft_pwd(void);
 void	ft_echo(t_cmd *cmd);
 
-
-
-#endif
+#endif /* builtins.h */
