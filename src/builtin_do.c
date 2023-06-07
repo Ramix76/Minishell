264a84f86484c166ff76e77219f353fa05c6282b
puyma <mpuig-ma@student.42barcelona.com>
2023-06-07 17:33:33 +0200

    function to execute builtins

---
+++ src/builtin_do.c
@@ -0,0 +1,23 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   builtin_do.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mpuig-ma <mpuig-ma@student.42barcel>       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/06/07 17:31:50 by mpuig-ma          #+#    #+#             */
+/*   Updated: 2023/06/07 17:33:15 by mpuig-ma         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "minishell.h"
+
+int	builtin_do(char *exec_name)
+{
+	(void) exec_name;
+	//if (isbuiltin(exec_name) == EXIT_SUCCESS)
+	// execute it
+	//else
+	//	return (EXIT_FAILURE);
+	return (EXIT_SUCCESS);
+}
