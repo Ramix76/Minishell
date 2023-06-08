cce86ee0521014661ed61ac2ec7a4afbba9a2216
puyma <mpuig-ma@student.42barcelona.com>
2023-06-08 12:57:41 +0200

    solved exit for now

--- src/builtin_do.c
+++ src/builtin_do.c
@@ -6,7 +6,7 @@
 /*   By: mpuig-ma <mpuig-ma@student.42barcel>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/07 17:31:50 by mpuig-ma          #+#    #+#             */
-/*   Updated: 2023/06/08 11:40:29 by mpuig-ma         ###   ########.fr       */
+/*   Updated: 2023/06/08 12:57:30 by mpuig-ma         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,11 +14,17 @@
 
 int	builtin_do(t_cmd *cmd, t_data *data)
 {
-	char	*exec_name;
+	const char	*exec_name = cmd->tokens[0];
 
-	exec_name = cmd->tokens[0];
-	(void) data;
-	(void) exec_name;
+	if (ft_strncmp(exec_name, "pwd", 3) == 0
+		|| ft_strncmp(exec_name, "PWD", 3) == 0)
+		ft_pwd();
+	else if (ft_strncmp(exec_name, "echo", 4) == 0
+		|| ft_strncmp(exec_name, "ECHO", 4) == 0)
+		ft_echo(cmd);
+	else if (ft_strncmp(exec_name, "env", 3) == 0
+		|| ft_strncmp(exec_name, "ENV", 3) == 0)
+		ft_env(data);
 	return (EXIT_SUCCESS);
 }
 
@@ -29,11 +35,14 @@ int	builtin_do(t_cmd *cmd, t_data *data)
 
 int	is_builtin(char *str)
 {
-	if (ft_strncmp(str, "pwd", 3) == 0)
+	if (ft_strncmp(str, "pwd", 3) == 0
+		|| ft_strncmp(str, "PWD", 3) == 0)
 		return (EXIT_SUCCESS);
-	else if (ft_strncmp(str, "echo", 4) == 0)
+	else if (ft_strncmp(str, "echo", 4) == 0
+		|| ft_strncmp(str, "ECHO", 4) == 0)
 		return (EXIT_SUCCESS);
-	else if (ft_strncmp(str, "env", 3) == 0)
+	else if (ft_strncmp(str, "env", 3) == 0
+		|| ft_strncmp(str, "ENV", 3) == 0)
 		return (EXIT_SUCCESS);
 	return (EXIT_FAILURE);
 }
