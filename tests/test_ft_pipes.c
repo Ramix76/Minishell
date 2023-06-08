9aed95f3b7397bad1de03abdcbfa0aa35d3f7bb0
puyma <mpuig-ma@student.42barcelona.com>
2023-06-08 18:48:30 +0200

    more tests

--- tests/test_ft_pipes.c
+++ tests/test_ft_pipes.c
@@ -6,7 +6,7 @@
 /*   By: mpuig-ma <mpuig-ma@student.42barcel>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/08 17:01:04 by mpuig-ma          #+#    #+#             */
-/*   Updated: 2023/06/08 17:07:57 by mpuig-ma         ###   ########.fr       */
+/*   Updated: 2023/06/08 18:40:34 by mpuig-ma         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,19 +17,23 @@
 int	pipe_do(char *line)
 {
 	char	**pipe_split;
+	char	*str;
 	int		i;
    
 	pipe_split = ft_split(line, '|');
 	i = 0;
 	while (pipe_split[i] != NULL)
 	{
+		str = ft_strtrim(pipe_split[i], " ");
+		free(pipe_split[i]);
+		pipe_split[i] = str;
 		ft_printf("%s\n", pipe_split[i]);
+		free(pipe_split[i]);
 		++i;
 	}
+	free(pipe_split);
 	return (EXIT_SUCCESS);
 }
-
-// missing strtrim
 
 int	main(int argc, char **argv, char **envp)
 {
