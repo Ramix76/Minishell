300fec0834aed17d78659ae86f28285a15bdf336
puyma <mpuig-ma@student.42barcelona.com>
2023-06-08 12:09:16 +0200

    add file

--- src/builtin_do.c
+++ src/builtin_do.c
@@ -6,14 +6,18 @@
 /*   By: mpuig-ma <mpuig-ma@student.42barcel>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/07 17:31:50 by mpuig-ma          #+#    #+#             */
-/*   Updated: 2023/06/07 17:35:50 by mpuig-ma         ###   ########.fr       */
+/*   Updated: 2023/06/08 11:40:29 by mpuig-ma         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
-int	builtin_do(char *exec_name)
+int	builtin_do(t_cmd *cmd, t_data *data)
 {
+	char	*exec_name;
+
+	exec_name = cmd->tokens[0];
+	(void) data;
 	(void) exec_name;
 	return (EXIT_SUCCESS);
 }
@@ -22,3 +26,14 @@ int	builtin_do(char *exec_name)
 	// execute it
 	//else
 	//	return (EXIT_FAILURE);
+
+int	is_builtin(char *str)
+{
+	if (ft_strncmp(str, "pwd", 3) == 0)
+		return (EXIT_SUCCESS);
+	else if (ft_strncmp(str, "echo", 4) == 0)
+		return (EXIT_SUCCESS);
+	else if (ft_strncmp(str, "env", 3) == 0)
+		return (EXIT_SUCCESS);
+	return (EXIT_FAILURE);
+}
