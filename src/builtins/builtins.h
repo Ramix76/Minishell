8e00d37393d5dcf66f25522df6f068ca26325311
puyma <mpuig-ma@student.42barcelona.com>
2023-06-08 12:08:43 +0200

    added file && func declaration

--- src/builtins/builtins.h
+++ src/builtins/builtins.h
@@ -6,20 +6,23 @@
 /*   By: framos-p <framos-p@student.42barcel>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/02 14:30:49 by framos-p          #+#    #+#             */
-/*   Updated: 2023/06/07 13:37:32 by mpuig-ma         ###   ########.fr       */
+/*   Updated: 2023/06/08 11:51:49 by mpuig-ma         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef BUILTINS_H
 # define BUILTINS_H
 
-# include "minishell.h"
+# include "../minishell.h"
 
 typedef struct s_cmd
 {
 	char	*command;
 	char	**tokens;
 }			t_cmd;
+
+int			builtin_do(t_cmd *cmd, t_data *data);
+int			is_builtin(char *str);
 
 int			ft_echo(t_cmd *cmd);
 int			ft_pwd(void);
