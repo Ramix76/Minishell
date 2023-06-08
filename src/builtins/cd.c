7bebb22689d575384a9788023e4b6aafe65d8220
Ramix76 <framos-p@car8s4.42barcelona.com>
2023-06-08 16:46:22 +0200

    finalized cd builtin

--- src/builtins/cd.c
+++ src/builtins/cd.c
@@ -6,7 +6,7 @@
 /*   By: framos-p <framos-p@student.42barcel>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/05 11:58:13 by framos-p          #+#    #+#             */
-/*   Updated: 2023/06/08 15:43:17 by mpuig-ma         ###   ########.fr       */
+/*   Updated: 2023/06/08 16:43:17 by framos-p         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -28,12 +28,12 @@ static void	change_to_parent_directory(void)
 		perror("No such file or directory\n");
 }
 
-static void	change_to_home_directory(void)
+static void	change_to_home_directory(t_data *data)
 {
 	char		path[PATH_MAX];
 	const char	*home_dir;
 
-	home_dir = getenv("HOME");
+	home_dir = ft_getenv("HOME", (const char **)data->envp);
 	if (home_dir == NULL)
 		perror("Error obtaining HOME directory\n");
 	if (chdir(home_dir) == 0)
@@ -47,12 +47,12 @@ static void	change_to_home_directory(void)
 		perror("Error changing directory\n");
 }
 
-static void	change_to_previous_directory(void)
+static void	change_to_previous_directory(t_data *data)
 {
 	char		path[PATH_MAX];
 	const char	*prev_dir;
 
-	prev_dir = getenv("OLDPWD");
+	prev_dir = ft_getenv("OLDPWD", (const char **)data->envp);
 	if (prev_dir == NULL)
 		perror("Error obtaining previous directory\n");
 	if (chdir(prev_dir) == 0)
@@ -83,22 +83,28 @@ static void	change_to_directory(const char *directory)
 
 int	ft_cd(t_cmd *cmd, t_data *data)
 {
-	char	path[PATH_MAX];
+	(void)data;
+	int	i;
 
-	if (argc != 2)
+	i = 0;
+	cmd->tokens_count = 0;
+	while(cmd->tokens[i] != NULL)
 	{
-		printf("Use: %s <directory's name>\n", argv[0]);
+		cmd->tokens_count++;
+		i++;
+	}
+	if (cmd->tokens_count - 1 != 1)
+	{
+		printf("Use: %s <directory's name>\n", cmd->tokens[0]);
 		return (EXIT_FAILURE);
 	}
-	if (strcmp(argv[1], "..") == 0)
+	if (ft_strncmp(cmd->tokens[1], "..", 2) == 0)
 		change_to_parent_directory();
-	else if (strcmp(argv[1], "~") == 0)
-		change_to_home_directory();
-	else if (strcmp(argv[1], "-") == 0)
-		change_to_previous_directory();
+	else if (ft_strncmp(cmd->tokens[1], "~", 1) == 0)
+		change_to_home_directory(data);
+	else if (ft_strncmp(cmd->tokens[1], "-", 1) == 0)
+		change_to_previous_directory(data);
 	else
-		change_to_directory(argv[1]);
-	if (getcwd(path, sizeof(path)) != NULL)
-		printf("Current directory: %s\n", path);
+		change_to_directory(cmd->tokens[1]);
 	return (EXIT_SUCCESS);
 }
