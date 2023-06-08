8584cc35060c5bd15b78b913cac0a0b49a28a42d
puyma <mpuig-ma@student.42barcelona.com>
2023-06-08 12:09:41 +0200

    created init_data

--- src/main.c
+++ src/main.c
@@ -6,44 +6,44 @@
 /*   By: framos-p <framos-p@student.42barcel>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/29 11:31:44 by framos-p          #+#    #+#             */
-/*   Updated: 2023/06/07 17:42:54 by mpuig-ma         ###   ########.fr       */
+/*   Updated: 2023/06/08 11:45:41 by mpuig-ma         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
-static int	init_args(int argc, char **argv, char **envp, t_data *data);
+static int	init_args(int argc, char **argv, char **envp);
+static int	init_data(int argc, char **argv, char **envp, t_data *data);
 
 int	main(int argc, char **argv, char **envp)
 {
 	t_data	data;
 
-	init_args(argc, argv, envp, &data);
+	init_args(argc, argv, envp);
+	init_data(argc, argv, envp, &data);
 	data.exit_code = shell_do(&data);
 	return (data.exit_code);
 }
 
-static int	init_args(int argc, char **argv, char **envp, t_data *data)
+static int	init_args(int argc, char **argv, char **envp)
 {
 	if (argc >= 2 && ft_strcmp(argv[1], "--version") == 0)
 	{
 		printf("%s\n", SH_VERSION);
 		exit(EXIT_SUCCESS);
 	}
-	data->envp = envp;
-	data->path = ft_getenv("PATH", (const char **) envp);
-	data->exit_code = 0;
+	(void) envp;
 	return (EXIT_SUCCESS);
 }
 
-void	free_str_arr(char **split)
+static int	init_data(int argc, char **argv, char **envp, t_data *data)
 {
-	int	i;
-
-	i = 0;
-	while (split[i] != NULL)
-	{
-		free(split[i]);
-		++i;
-	}
+	(void) argc;
+	(void) argv;
+	data->envp = envp;
+	data->path = ft_getenv("PATH", (const char **) envp);
+	if (data->path == NULL)
+		data->path = _PATH_DEFPATH;
+	data->exit_code = 0;
+	return (EXIT_SUCCESS);
 }
