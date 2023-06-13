b79830de031a835d464c38d2b7118b0c4a09984f
puyma <mpuig-ma@student.42barcelona.com>
2023-06-13 12:17:27 +0200

    some more tests

--- tests/test_ft_setenv.c
+++ tests/test_ft_setenv.c
@@ -6,103 +6,12 @@
 /*   By: mpuig-ma <mpuig-ma@student.42barcel>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/12 17:52:45 by mpuig-ma          #+#    #+#             */
-/*   Updated: 2023/06/12 19:50:58 by mpuig-ma         ###   ########.fr       */
+/*   Updated: 2023/06/13 12:14:57 by mpuig-ma         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 #include "test.utils.h"
-
-int	ft_setenv(const char *name, const char *value, int overwrite, const char **envp, t_data *data);
-static int	init_env(char **envp, t_data *data);
-
-static int	add(const char *name, const char *value, char **envp, t_data *data);
-static int	rm(const char *name, char **envp);
-static int	overwrite(const char *name, const char *value, const char **envp, t_data *data);
-
-int	main(int argc, char **argv, char **envp)
-{
-	t_data	data;
-
-	init_log(argc, argv, envp);
-	init_env(envp, &data);
-	rm("TERM_PROGRAM", envp);
-	return (0);
-}
-
-static int	rm(const char *name, char **envp)
-{
-	int		len;
-	char	*var_name;
-
-	len = 0;
-	var_name = ft_strjoin(name, "=");
-
-	while (envp[len] != NULL)
-	{
-		if (ft_strncmp(envp[len], var_name, ft_strlen(var_name)) == 0)
-		{
-			free(envp[len]);
-			++i;
-		}
-		ft_printf("%s\n", envp[len]);
-
-		++i;
-		++len;
-	}
-	free(var_name);
-	return (EXIT_SUCCESS);
-}
-
-int	ft_setenv(const char *name, const char *value, int owr, const char **envp, t_data *data)
-{
-	char	*env_name;
-
-	env_name = NULL;
-	if (name == NULL)
-		return (EXIT_FAILURE);
-	else if (name != NULL && owr == 0)
-		return (EXIT_SUCCESS);
-	env_name = ft_getenv(name, envp);
-	if (env_name == NULL)
-	{
-		add(name, value, (char **) envp, data);
-	}
-	else
-	{
-		if (owr != 0)
-			overwrite(name, value, envp, data);
-	}
-	return (EXIT_SUCCESS);
-}
-
-static int	add(const char *name, const char *value, char **envp, t_data *data)
-{
-	int		len;
-	char	**new_envp;
-
-	len = 0;
-	new_envp = NULL;
-	while (envp[len] != NULL)
-		++len;
-	new_envp = (char **) realloc(envp, sizeof(char *) * (len + 2));
-	if (new_envp == NULL)
-		return (EXIT_FAILURE);
-	new_envp[len] = (char *) name;
-	(void) value;
-	new_envp[len + 1] = NULL;
-	data->envp = new_envp;
-	return (EXIT_SUCCESS);
-}
-
-static int	overwrite(const char *name, const char *value, const char **envp, t_data *data)
-{
-	(void) name;
-	(void) value;
-	(void) envp;
-	(void) data;
-	return (EXIT_SUCCESS);
-}
 
 static int	init_env(char **envp, t_data *data)
 {
@@ -120,4 +29,26 @@ static int	init_env(char **envp, t_data *data)
 	new_envp[len] = NULL;
 	data->envp = new_envp;
 	return (EXIT_SUCCESS);
+}
+
+void	print_env(char **envp)
+{
+	int	i;
+
+	i = 0;
+	while (envp[i] != NULL)
+		ft_printf("  %s\n", envp[i++]);
+}
+
+int	main(int argc, char **argv, char **envp)
+{
+	t_data	data;
+
+	init_log(argc, argv, envp);
+	init_env(envp, &data);
+	if (ft_setenv("TEST", "42", 1, (const char **) data.envp) == EXIT_FAILURE)
+		ft_fprintf(stderr, "ft_setenv failed\n");
+	if (ft_setenv("TEST", "0042", 1, (const char **) data.envp) == EXIT_FAILURE)
+		ft_fprintf(stderr, "ft_setenv failed\n");
+	print_env(data.envp);
 }
