3556dc3106219e034a41a7afef2528ae2bc472bd
puyma <mpuig-ma@student.42barcelona.com>
2023-06-15 17:24:06 +0200

    still

--- src/command_do.c
+++ src/command_do.c
@@ -6,7 +6,7 @@
 /*   By: mpuig-ma <mpuig-ma@student.42barcel>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/07 11:56:40 by mpuig-ma          #+#    #+#             */
-/*   Updated: 2023/06/15 17:12:52 by mpuig-ma         ###   ########.fr       */
+/*   Updated: 2023/06/15 17:15:34 by mpuig-ma         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,27 +19,29 @@
 int	command_do(char *line, t_data *data)
 {
 	t_cmd	cmd;
-	char	*cmd_str;
+	char	*cmd_path;
 
 	line = shell_expand(line, data);
 	cmd.tokens = ft_split(line, ' ');
 	if (cmd.tokens[0] == NULL)
 		return (free_str_arr(cmd.tokens), EXIT_SUCCESS);
-	cmd_str = ft_which(cmd.tokens[0], data->path);
+	cmd_path = ft_which(cmd.tokens[0], data->path);
 	if (is_builtin(cmd.tokens[0]) == EXIT_SUCCESS)
 		builtin_do(&cmd, data);
 	else if (ft_strncmp(cmd.tokens[0], "exit", 4) == 0)
 	{
-		free(cmd_str);
+		free(line);
+		free(cmd_path);
 		free_str_arr(cmd.tokens);
 		exit (EXIT_SUCCESS);
 	}
-	else if (cmd_str == NULL)
+	else if (cmd_path == NULL)
 		ft_fprintf(stderr, "%s: %s: command not found\n",
 			SH_NAME, cmd.tokens[0]);
 	else
 		pipe_do(line, data);
-	free(cmd_str);
+	free(line);
+	free(cmd_path);
 	free_str_arr(cmd.tokens);
 	return (EXIT_SUCCESS);
 }
--- src/shell_expand.c
+++ src/shell_expand.c
@@ -6,7 +6,7 @@
 /*   By: mpuig-ma <mpuig-ma@student.42barcel>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/05 16:45:44 by mpuig-ma          #+#    #+#             */
-/*   Updated: 2023/06/15 17:12:31 by mpuig-ma         ###   ########.fr       */
+/*   Updated: 2023/06/15 17:22:18 by mpuig-ma         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,7 +17,7 @@ char	*shell_expand(char *line, t_data *data)
 	char	*expanded;
 
 	(void) data;
+	(void) line;
 	expanded = ft_strdup(line);
-	expanded = "echo 0";
 	return (expanded);
 }
