1a1ec653880a3419c813d3805fd2e21135394e54
puyma <mpuig-ma@student.42barcelona.com>
2023-06-15 17:01:30 +0200

    manage exit code for single command

--- src/pipe_do.c
+++ src/pipe_do.c
@@ -6,14 +6,14 @@
 /*   By: mpuig-ma <mpuig-ma@student.42barcel>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/15 13:29:41 by mpuig-ma          #+#    #+#             */
-/*   Updated: 2023/06/15 16:21:16 by mpuig-ma         ###   ########.fr       */
+/*   Updated: 2023/06/15 16:59:38 by mpuig-ma         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
 static int	pipex(char **argv, t_data *data);
-static void	execute_command(char *argv, char **envp, int *fd);
+static int	execute_command(char *argv, char **envp, int *fd);
 
 int	pipe_do(char *line, t_data *data)
 {
@@ -40,18 +40,19 @@ static int	pipex(char **argv, t_data *data)
 	{
 		//dup2(fd, STDIN_FILENO);
 		//close(fd);
-		execute_command(argv[i], data->envp, &fd);
+		data->exit_code = execute_command(argv[i], data->envp, &fd);
 		++i;
 	}
 	//close(fd);
 	return (EXIT_SUCCESS);
 }
 
-static void	execute_command(char *argv, char **envp, int *fd)
+static int	execute_command(char *argv, char **envp, int *fd)
 {
-	int		fildes[2];
-	char	**cmd;
-	pid_t	pid;
+	int			fildes[2];
+	char		**cmd;
+	pid_t		pid;
+	int			exit_code;
 
 	if (pipe(fildes) == -1)
 		exit (4);
@@ -68,10 +69,13 @@ static void	execute_command(char *argv, char **envp, int *fd)
 		//close(fildes[RD]);
 		//close(fildes[WR]);
 		ft_execvpe(cmd[0], (char const **) cmd, (const char **) envp);
-		exit (1);
+		exit (EXIT_FAILURE);
 	}
-	waitpid(pid, NULL, 0);
-	//close(fildes[WR]);
-	//*fd = fildes[RD];
-	(void) fd;
+	waitpid(pid, &exit_code, 0);
+	if (WIFEXITED(exit_code))
+		exit_code = WEXITSTATUS(exit_code);
+	close(fildes[WR]);
+	*fd = fildes[RD];
+	ft_printf("exit_code: %d\n", exit_code);
+	return (exit_code);
 }
