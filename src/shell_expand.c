c7fa519e97a35de055676bae706b67114dbb2e5e
puyma <mpuig-ma@student.42barcelona.com>
2023-07-11 16:06:20 +0200

    norm (also: backup)

--- src/shell_expand.c
+++ src/shell_expand.c
@@ -6,41 +6,59 @@
 /*   By: mpuig-ma <mpuig-ma@student.42barcel>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/05 16:45:44 by mpuig-ma          #+#    #+#             */
-/*   Updated: 2023/07/11 12:29:30 by mpuig-ma         ###   ########.fr       */
+/*   Updated: 2023/07/11 16:03:53 by mpuig-ma         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
-/*
- * name: A word consisting solely of letters, numbers, and underscores, 
- * and beginning with a letter or underscore. 
- * Names are used as shell variable and function names.
- * Also referred to as an identifier.
- *
- * ft_getname(char *ptr)
- * returns name portion from string
- *
- */
+static char	*ft_getname(char *ptr);
+char		*ft_getvar(char	*ptr, t_data *data);
+char		*ft_expand_dollar(char *expanded, char *dollar, t_data *data);
 
-char	*ft_getname(char *ptr)
+char	*ft_shell_expand(char *line, t_data *data)
 {
-	char	*name;
-	size_t	name_len;
+	char	*expanded;
+	char	*dollar;
+	char	*temp;
 
-	if (ptr == NULL || *ptr == '\0' || *ptr != '$')
-		return (NULL);
-	++ptr;
-	name_len = 0;
-	if (*ptr == '\0' && (ft_isalpha(*ptr) == 0 || *ptr != '_'))
-		return (NULL);
-	while (ptr[name_len] != '\0' && (ft_isalnum(ptr[name_len]) != 0
-			|| ptr[name_len] == '_'))
-		++name_len;
-	name = ft_strndup(ptr, name_len);
-	return (name);
+	expanded = ft_strdup(line);
+	dollar = ft_strchr(expanded, '$');
+	while (dollar != NULL)
+	{
+		temp = ft_expand_dollar(expanded, dollar, data);
+		if (temp != expanded)
+			free(expanded);
+		expanded = temp;
+		dollar = ft_strchr(expanded, '$');
+	}
+	return (expanded);
 }
 
+char	*ft_expand_dollar(char *expanded, char *dollar, t_data *data)
+{
+	char	*name;
+	char	*value;
+	char	*temp;
+	size_t	name_len;
+	size_t	p;
+
+	name = ft_getname(dollar);
+	if (name != NULL)
+	{
+		name_len = ft_strlen(name);
+		value = ft_getvar(name, data);
+		ft_memmove(dollar + 1, dollar + 1 + name_len,
+			ft_strlen(dollar + name_len));
+		p = ft_strchr(expanded, '$') - expanded;
+		temp = ft_strpjoin_replace(expanded, value, p);
+		expanded = temp;
+		free(name);
+	}
+	return (expanded);
+}
+
+/*
 char	*ft_shell_expand(char *line, t_data *data)
 {
 	char	*expanded;
@@ -73,4 +91,53 @@ char	*ft_shell_expand(char *line, t_data *data)
 		dollar = ft_strchr(expanded, '$');
 	}
 	return (expanded);
+}
+*/
+
+/*
+ * As in GNU's Bash Reference:
+ * name: A word consisting solely of letters, numbers, and underscores,
+ * and beginning with a letter or underscore.
+ * Names are used as shell variable and function names.
+ * Also referred to as an identifier.
+ *
+ * ft_getname(char *ptr)
+ * returns name portion from string
+ *
+ */
+
+static char	*ft_getname(char *ptr)
+{
+	char	*name;
+	size_t	name_len;
+
+	if (ptr == NULL || *ptr == '\0' || *ptr != '$')
+		return (NULL);
+	++ptr;
+	name_len = 0;
+	if (*ptr == '\0' && (ft_isalpha(*ptr) == 0 || *ptr != '_'))
+		return (NULL);
+	while (ptr[name_len] != '\0' && (ft_isalnum(ptr[name_len]) != 0
+			|| ptr[name_len] == '_'))
+		++name_len;
+	name = ft_strndup(ptr, name_len);
+	return (name);
+}
+
+/*
+ * Function gets variable's (word) pointed by ptr value.
+ *
+ * ft_getvar(char *name, t_data *data)
+ * no need to free
+ *
+ */
+
+char	*ft_getvar(char	*name, t_data *data)
+{
+	char	*value;
+
+	value = ft_getenv(name, (const char **) data->envp);
+	if (value == NULL)
+		value = "";
+	return (value);
 }
