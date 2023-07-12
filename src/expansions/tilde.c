abbdb1fc5681752b5d869b4fbc2e0031f410bb96
puyma <mpuig-ma@student.42barcelona.com>
2023-07-12 12:25:34 +0200

    starting tilde shit

--- src/expansions/tilde.c
+++ src/expansions/tilde.c
@@ -6,7 +6,7 @@
 /*   By: mpuig-ma <mpuig-ma@student.42barcel>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/11 16:34:37 by mpuig-ma          #+#    #+#             */
-/*   Updated: 2023/07/11 16:40:01 by mpuig-ma         ###   ########.fr       */
+/*   Updated: 2023/07/12 12:24:41 by mpuig-ma         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,9 +15,28 @@
 char	*ft_expand_tilde(char *line, t_data *data)
 {
 	char	*expanded;
+	char	*tilde;
 
-	(void) line;
 	(void) data;
-	expanded = NULL;
+	expanded = ft_strdup(line);
+	tilde = ft_strchr(expanded, '~');
+	while (tilde != NULL)
+	{
+		tilde = ft_strchr(expanded, '~');
+	}
 	return (expanded);
 }
+
+/*
+int	main(void)
+{
+	char	*line;
+	char	*expanded;
+
+	line = "expand this shit ~";
+	expanded = ft_expand_tilde(line, NULL);
+	printf("%s\n", expanded);
+	free(expanded);
+	return (0);
+}
+*/
