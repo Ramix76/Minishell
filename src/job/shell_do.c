acb32722c31394d057ce2351502d1f279ee080e1
puyma <mpuig-ma@student.42barcelona.com>
2023-07-25 16:15:40 +0200

    rm unused comment

--- src/job/shell_do.c
+++ src/job/shell_do.c
@@ -6,7 +6,7 @@
 /*   By: mpuig-ma <mpuig-ma@student.42barcel>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/07 11:56:15 by mpuig-ma          #+#    #+#             */
-/*   Updated: 2023/07/24 12:11:01 by mpuig-ma         ###   ########.fr       */
+/*   Updated: 2023/07/25 16:13:07 by mpuig-ma         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -36,24 +36,3 @@ int	ft_shell_do(t_data *data)
 	rl_clear_history();
 	return (EXIT_SUCCESS);
 }
-
-/*int	ft_shell_do(t_data *data)
-{
-	char	*line;
-	char	*parsed_line;
-
-	line = readline(PROMPT);
-	while (g_running && (line = readline(PROMPT)) != NULL)
-	{
-		if (line[0] != '\0')
-			add_history(line);
-		parsed_line = ft_shell_expand(line, data);
-		free(line);
-		data->exit_code = ft_job_control(parsed_line, data);
-		free(parsed_line);
-		rl_on_new_line();
-		line = readline(PROMPT);
-	}
-	rl_clear_history();
-	return (EXIT_SUCCESS);
-}*/
