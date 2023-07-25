3081074fc0cfdaf0d2f8f4474e9c9ec0c10cdb8a
puyma <mpuig-ma@student.42barcelona.com>
2023-07-25 14:01:26 +0200

    temporal fix, arguments not solved

--- src/job/job_control.c
+++ src/job/job_control.c
@@ -6,7 +6,7 @@
 /*   By: mpuig-ma <mpuig-ma@student.42barcel>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/20 11:43:35 by mpuig-ma          #+#    #+#             */
-/*   Updated: 2023/07/24 12:37:26 by mpuig-ma         ###   ########.fr       */
+/*   Updated: 2023/07/25 13:57:49 by mpuig-ma         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -68,8 +68,13 @@ int	ft_command_do(char *line, t_data *data)
 	cmd.tokens = ft_split(line, ' ');
 	if (cmd.tokens[0] == NULL)
 		return (ft_free_str_arr(cmd.tokens), EXIT_SUCCESS);
-	cmd_path = ft_which(cmd.tokens[0], data->path);
-	cmd_path = ft_realpath(cmd.tokens[0], NULL);
+
+
+	if (cmd.tokens[0][0] != '.' && cmd.tokens[0][0] != '/')
+		cmd_path = ft_which(cmd.tokens[0], data->path);
+	else
+		cmd_path = ft_realpath(cmd.tokens[0], NULL);
+	
 	if (ft_is_builtin(cmd.tokens[0]) == EXIT_SUCCESS)
 		ft_builtin_do(&cmd, data);
 	else if (ft_strncmp(cmd.tokens[0], "exit", 4) == 0)
@@ -91,6 +96,13 @@ int	ft_command_do(char *line, t_data *data)
 	ft_free_str_arr(cmd.tokens);
 	return (EXIT_SUCCESS);
 }
+
+/*
+ * ft_pipe_do (char *line, t_data *data)
+ *
+ * 		should receive a t_cmd *cmd instead of a char *line.
+ *
+ */
 
 int	ft_pipe_do(char *line, t_data *data)
 {
