116a157e0106e4de07b3088d6a8b48b3a2745ae7
Ramix76 <framos-p@car8s4.42barcelona.com>
2023-07-26 17:26:17 +0200

    la funcio ja neteja el archius ammb '.'

--- src/wildcard/wildcard.c
+++ src/wildcard/wildcard.c
@@ -6,13 +6,60 @@
 /*   By: framos-p <framos-p@student.42barcel>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/24 14:48:38 by framos-p          #+#    #+#             */
-/*   Updated: 2023/07/24 16:44:27 by framos-p         ###   ########.fr       */
+/*   Updated: 2023/07/26 17:16:36 by framos-p         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
 #define MAX_STRING_LENGTH 10000
+
+static int	ft_is_hidden_file(const char *name);
+static char	*ft_allocate_and_copy_name(const char *src);
+static void	ft_add_to_result(char ***result, size_t *count, const char *name);
+
+static int	ft_is_hidden_file(const char *name)
+{
+	return (name[0] == '.' && ft_strcmp(name, ".") != 0
+		&& ft_strcmp(name, "..") != 0);
+}
+
+static char	*ft_allocate_and_copy_name(const char *src)
+{
+	char	*name;
+	size_t	src_len;
+
+	src_len = ft_strlen(src);
+	name = (char *)malloc(src_len + 1);
+	if (name == NULL)
+	{
+		ft_fprintf(stderr, "Memory allocation error\n");
+		return (NULL);
+	}
+	else
+		ft_strlcpy(name, src, src_len + 1);
+	return (name);
+}
+
+static void	ft_add_to_result(char ***result, size_t *count, const char *name)
+{
+	char	**temp;
+
+	temp = (char **)ft_realloc(*result,
+			(*count + 1) * sizeof(char *), (*count) * sizeof(char *));
+	if (temp != NULL)
+	{
+		*result = temp;
+		(*result)[*count] = ft_allocate_and_copy_name(name);
+		if ((*result)[*count] != NULL)
+		{
+			(*count)++;
+			(*result)[*count] = NULL;
+		}
+	}
+	else
+		ft_fprintf(stderr, "Memory reallocation error\n");
+}
 
 char	**ft_files_and_dirs(void)
 {
@@ -22,46 +69,21 @@ char	**ft_files_and_dirs(void)
 	size_t			count;
 	char			*name;
 
-	count = 0;
+	result = NULL;
 	dir = opendir(".");
-	if (dir != NULL)
+	if (dir == NULL)
+		return (ft_fprintf(stderr, "Error opening dir\n"), NULL);
+	entry = readdir(dir);
+	while (entry != NULL)
 	{
-		entry = readdir(dir);
-		while (entry != NULL)
+		if (!ft_is_hidden_file(entry->d_name))
 		{
-			if (ft_strcmp(entry->d_name, ".") != 0 
-				&& ft_strcmp(entry->d_name, "..") != 0)
-			{
-				name = (char *)malloc(ft_strlen(entry->d_name) + 1);
-				if (name == NULL)
-				{
-					ft_fprintf(stderr, "Memory allocation error\n");
-					closedir(dir);
-					ft_free_str_arr(result);
-					return (NULL);
-				}
-				ft_strlcpy(name, entry->d_name, ft_strlen(entry->d_name) + 1);
-				count++;
-				result = (char **)ft_realloc(result,
-						(count + 1) * sizeof(char *), count * sizeof(char *));
-				if (result == NULL)
-				{
-					ft_fprintf(stderr, "Memory allocation error\n");
-					closedir(dir);
-					free(name);
-					ft_free_str_arr(result);
-					return (NULL);
-				}
-				result[count - 1] = name;
-				result[count] = NULL;
-			}
+			name = ft_allocate_and_copy_name(entry->d_name);
+			if (name != NULL)
+				ft_add_to_result(&result, &count, name);
 		}
-		closedir(dir);
+		entry = readdir(dir);
 	}
-	else 
-	{
-		ft_fprintf(stderr, "Error al abrir el directorio\n");
-		return (NULL);
-	}
+	closedir(dir);
 	return (result);
 }
