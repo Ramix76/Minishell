34f2da1e16a44705685c204e0d598a84b289b922
puyma <mpuig-ma@student.42barcelona.com>
2023-07-28 13:02:27 +0200

    fix: refactor using ft_setenv

--- src/builtins/export.c
+++ src/builtins/export.c
@@ -12,88 +12,15 @@
 
 #include "minishell.h"
 
-static void	ft_add_exported_var(const char *var, t_data *data);
-static void	ft_process_variable_without_value(const char *var, t_data *data);
-static void	ft_process_variable_with_value(const char *var, t_data *data);
-static void	ft_process_export_token(char *token, t_data *data);
+/* 
+ * MISSING:
+ * check if name is already in data->envp or data->exported_vars!
+ *
+ */
 
-static void	ft_add_exported_var(const char *var, t_data *data)
-{
-	int		count;
-	char	**new_exported_vars;
-	char	*temp;
+static int	ft_process_export_token(char *token, t_data *data);
 
-	count = 0;
-	while (data->exported_vars && data->exported_vars[count] != NULL)
-	{
-		if (strcmp(data->exported_vars[count], var) == 0)
-			return ;
-		count++;
-	}
-	new_exported_vars = ft_realloc(data->exported_vars,
-			sizeof(char *) * (count + 1), (count + 2));
-	if (new_exported_vars == NULL)
-	{
-		errno = ENOMEM;
-		perror(SH_NAME);
-		return ;
-	}
-	data->exported_vars = new_exported_vars;
-
-	(void) temp;	
-	//temp = ft_getenv(var, (const char **) data->exported_vars);
-	//printf("temp: %s\n", temp);
-
-	data->exported_vars[count] = ft_strdup(var);
-	data->exported_vars[count + 1] = NULL;
-}
-
-static void	ft_process_variable_without_value(const char *var, t_data *data)
-{
-	//data->exported_vars = (char **) malloc(sizeof(char *) * 1);
-	//data->exported_vars[0] = (char *) malloc(sizeof(char *) * 1);
-	printf("var: %s\n", var);
-	ft_setenv(var, "hey", 1, &data->exported_vars);
-	//ft_add_exported_var(var, data);
-	(void) ft_add_exported_var;
-}
-
-static void	ft_process_variable_with_value(const char *var, t_data *data)
-{
-	char	*equal_sign;
-	size_t	name_length;
-	char	*name;
-	char	*value;
-
-	equal_sign = ft_strchr(var, '=');
-	name_length = equal_sign - var;
-	name = ft_strndup(var, name_length);
-	value = equal_sign + 1;
-	if (!ft_is_valid_var_format(name))
-	{
-		ft_error(ERR_FORMAT, "export", name);
-		free(name);
-		return ;
-	}
-	ft_setenv(name, value, 1, &data->envp);
-	free(name);
-}
-
-static void	ft_process_export_token(char *token, t_data *data)
-{
-	char	*equal_sign;
-// posa aqui el cas de += //
-	equal_sign = ft_strchr(token, '=');
-	ft_setenv(token, token, 1, &data->exported_vars);
-	//if (equal_sign == NULL || *(equal_sign + 1) == '\0')
-	//	ft_process_variable_without_value(token, data);
-	//else
-	//	ft_process_variable_with_value(token, data);
-	(void) ft_process_variable_without_value;
-	(void) ft_process_variable_with_value;
-}
-
-void	ft_export(t_cmd *cmd, t_data *data)
+int	ft_export(t_cmd *cmd, t_data *data)
 {
 	int	count;
 	int	i;
@@ -104,9 +31,38 @@ void	ft_export(t_cmd *cmd, t_data *data)
 	if (count == 1)
 	{
 		ft_print_combined_vars(data);
-		return ;
+		return (EXIT_SUCCESS);
 	}
 	i = 0;
 	while (++i < count)
 		ft_process_export_token(cmd->tokens[i], data);
+	return (EXIT_SUCCESS);
+}
+
+static int	ft_process_export_token(char *token, t_data *data)
+{
+	char	*equal_sign;
+	char	*name;
+	char	*value;
+
+	equal_sign = ft_strchr(token, '=');
+	if (equal_sign == NULL || *(equal_sign + 1) == '\0')
+	{
+		ft_setenv(token, "", 1, &data->exported_vars);
+		return (EXIT_SUCCESS);
+	}
+	if (ft_strnstr(token, "+=", ft_strlen(token)) != NULL
+		&& ft_strnstr(token, "+=", ft_strlen(token)) + 1 == equal_sign)
+	{
+		name = ft_strndup(token, ft_strchr(token, '=') - token - 1);
+		value = ft_strchr(token, '=') + 1;
+	}
+	else
+	{
+		name = ft_strndup(token, ft_strchr(token, '=') - token);
+		value = ft_strchr(token, '=') + 1;
+	}
+	ft_setenv(name, value, 1, &data->envp);
+	free(name);
+	return (EXIT_SUCCESS);
 }
