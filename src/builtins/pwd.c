6a8b4b8c912e51b15b95eb8733a6e907c0e450d3
Ramix76 <framos-p@car8s4.42barcelona.com>
2023-07-31 13:14:57 +0200

    fixed error format

--- src/builtins/pwd.c
+++ src/builtins/pwd.c
@@ -6,7 +6,7 @@
 /*   By: framos-p <framos-p@student.42barcel>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/29 12:54:40 by framos-p          #+#    #+#             */
-/*   Updated: 2023/07/29 17:24:21 by framos-p         ###   ########.fr       */
+/*   Updated: 2023/07/31 13:11:03 by framos-p         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,5 +23,6 @@ int	ft_pwd(t_data *data)
 		return (EXIT_SUCCESS);
 	}
 	else
-		return (ft_fprintf(stderr,"%s: pwd: Failed to get current directory"), EXIT_FAILURE);
+		return (ft_fprintf(stderr, "%s: pwd: Failed to get current directory"),
+			EXIT_FAILURE);
 }
