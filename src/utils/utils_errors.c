12c711b4fdfa05016186ad4e93188f2c7904b9e0
Ramix76 <framos-p@car8s4.42barcelona.com>
2023-07-31 13:17:28 +0200

    refactor ft_error function

--- src/utils/utils_errors.c
+++ src/utils/utils_errors.c
@@ -6,31 +6,15 @@
 /*   By: framos-p <framos-p@student.42barcel>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/12 12:21:08 by framos-p          #+#    #+#             */
-/*   Updated: 2023/07/29 17:34:49 by framos-p         ###   ########.fr       */
+/*   Updated: 2023/07/31 12:11:10 by framos-p         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
-void	ft_error(int error, const char *command, const char *dir)
+void	ft_error(const char *comd, const char *dir)
 {
-	const char	*error_msg;
-
-	error_msg = NULL;
-	if (error == ENOENT || error == EACCES || error == ENOTDIR)
-		error_msg = strerror(error);
-	else if (error == NO_HOME)
-		error_msg = NO_HOME_MSG;
-	else if (error == ERR_CD)
-		error_msg = ERR_CD_MSG;
-	else if (error == ERR_PWD)
-		error_msg = ERR_PWD_MSG;
-	else if (error == ERR_OPWD)
-		error_msg = ERR_OPWD_MSG;
-	else if (error == ERR_FORMAT)
-		error_msg = ERR_FORMAT_MSG;
-	if (error_msg != NULL)
-		ft_fprintf(stderr, "%s: %s: %s: %s\n", SH_NAME, command, dir, error_msg);
+	ft_fprintf(stderr, "%s: %s: %s: %s\n", SH_NAME, comd, dir, strerror(errno));
 }
 
 int	ft_cd_check_arguments(t_cmd *cmd)
