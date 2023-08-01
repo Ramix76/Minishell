553cfdd3b7eae21546a9e990e1c8318fb0841210
puyma <mpuig-ma@student.42barcelona.com>
2023-08-01 12:33:10 +0200

    fix: return builtin\'s return

--- src/builtins/builtin_do.c
+++ src/builtins/builtin_do.c
@@ -6,7 +6,7 @@
 /*   By: mpuig-ma <mpuig-ma@student.42barcel>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/07 17:31:50 by mpuig-ma          #+#    #+#             */
-/*   Updated: 2023/07/31 13:36:03 by mpuig-ma         ###   ########.fr       */
+/*   Updated: 2023/08/01 12:19:30 by mpuig-ma         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,16 +17,16 @@ int	ft_builtin_do(t_cmd *cmd, t_data *data)
 	const char	*exec_name = cmd->tokens[0];
 
 	if (ft_strcmp("pwd", exec_name) == 0)
-		ft_pwd(data);
+		return (ft_pwd(data));
 	else if (ft_strcmp("echo", exec_name) == 0)
-		ft_echo(cmd);
+		return (ft_echo(cmd));
 	else if (ft_strcmp("cd", exec_name) == 0)
-		ft_cd(cmd, data);
+		return (ft_cd(cmd, data));
 	else if (ft_strcmp("env", exec_name) == 0)
-		ft_env(data);
+		return (ft_env(data));
 	else if (ft_strcmp("unset", exec_name) == 0)
-		ft_unset(cmd, data);
+		return (ft_unset(cmd, data));
 	else if (ft_strcmp("export", exec_name) == 0)
-		ft_export(cmd, data);
-	return (EXIT_SUCCESS);
+		return (ft_export(cmd, data));
+	return (EXIT_FAILURE);
 }
