31ea883a9410915e79a50e2c3f3c4315d45998b3
puyma <mpuig-ma@student.42barcelona.com>
2023-08-03 13:03:14 +0200

    redo

--- src/expansions/tilde.c
+++ src/expansions/tilde.c
@@ -6,31 +6,23 @@
 /*   By: mpuig-ma <mpuig-ma@student.42barcel>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/11 16:34:37 by mpuig-ma          #+#    #+#             */
-/*   Updated: 2023/08/03 11:03:42 by mpuig-ma         ###   ########.fr       */
+/*   Updated: 2023/08/03 12:33:00 by mpuig-ma         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
-char	*ft_expand_tilde(char *line, t_data *data)
+char	*ft_expand_tilde(char *str, t_data *data)
 {
-	char	*expanded;
 	char	*temp;
-	char	*tilde;
-	size_t	p;
+	char	*expanded;
 
-	if (data->home == NULL)
-		data->home = "";
-	expanded = ft_strdup(line);
-	tilde = ft_strchr(expanded, '~');
-	while (tilde != NULL)
+	expanded = ft_strdup(str);
+	if (str != NULL && *str == 0176)
 	{
-		p = ft_strchr(expanded, '~') - expanded;
-		temp = ft_strpjoin_replace(expanded, data->home, p);
-		free(expanded);
-		expanded = temp;
-		tilde = ft_strchr(expanded, '~');
+		temp = expanded;
+		expanded = ft_strpjoin_replace(expanded, data->home, 0);
+		free(temp);
 	}
-	free(line);
 	return (expanded);
 }
