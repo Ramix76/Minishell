aa604e4ff6778dbab4b1945adb1f58e5f85cc94d
puyma <mpuig-ma@student.42barcelona.com>
2023-08-05 18:31:39 +0200

    success exit code

--- src/builtins/pwd.c
+++ src/builtins/pwd.c
@@ -6,7 +6,7 @@
 /*   By: framos-p <framos-p@student.42barcel>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/29 12:54:40 by framos-p          #+#    #+#             */
-/*   Updated: 2023/07/31 16:10:05 by framos-p         ###   ########.fr       */
+/*   Updated: 2023/08/05 18:27:44 by mpuig-ma         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,10 +16,10 @@ int	ft_pwd(t_data *data)
 {
 	char	cwd[PATH_MAX];
 
-	(void)data;
 	if (getcwd(cwd, PATH_MAX) != NULL)
 	{
 		printf("%s\n", cwd);
+		data->exit_code = EXIT_SUCCESS;
 		return (EXIT_SUCCESS);
 	}
 	ft_fprintf(stderr, "%s: pwd: Failed to get current directory\n", SH_NAME);
