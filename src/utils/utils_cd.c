7f89f1ad99aaa8f92fc4191eacd9b063d701c8b9
puyma <mpuig-ma@student.42barcelona.com>
2023-08-07 18:10:04 +0200

    mv ft for coherence

--- src/utils/utils_cd.c
+++ src/utils/utils_cd.c
@@ -19,3 +19,17 @@ int	ft_cd_ultra_specific_situation(void)
 		strerror(errno));
 	return (EXIT_FAILURE);
 }
+
+int	ft_cd_check_arguments(t_cmd *cmd)
+{
+	int			i;
+
+	i = -1;
+	cmd->tokens_count = 0;
+	while (cmd->tokens[++i] != NULL)
+		cmd->tokens_count++;
+	if (cmd->tokens_count - 1 > 2)
+		return (ft_fprintf(stderr, "%s: %s: No such file or directory\n",
+				SH_NAME, cmd->tokens[0], EXIT_FAILURE));
+	return (EXIT_SUCCESS);
+}
--- src/utils/utils_errors.c
+++ src/utils/utils_errors.c
@@ -16,17 +16,3 @@ void	ft_error(const char *comd, const char *dir)
 {
 	ft_fprintf(stderr, "%s: %s: %s: %s\n", SH_NAME, comd, dir, strerror(errno));
 }
-
-int	ft_cd_check_arguments(t_cmd *cmd)
-{
-	int			i;
-
-	i = -1;
-	cmd->tokens_count = 0;
-	while (cmd->tokens[++i] != NULL)
-		cmd->tokens_count++;
-	if (cmd->tokens_count - 1 > 2)
-		return (ft_fprintf(stderr, "%s: %s: No such file or directory\n",
-				SH_NAME, cmd->tokens[0], EXIT_FAILURE));
-	return (EXIT_SUCCESS);
-}
