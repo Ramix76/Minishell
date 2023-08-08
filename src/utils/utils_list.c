386d582750e0b9fff42df33f39a5af526493bc4e
Ramix76 <framos-p@car8s4.42barcelona.com>
2023-08-08 11:28:05 +0200

    fixed sort_list wildcard

--- src/utils/utils_list.c
+++ src/utils/utils_list.c
@@ -6,11 +6,13 @@
 /*   By: mpuig-ma <mpuig-ma@student.42barcel>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/07 15:49:37 by mpuig-ma          #+#    #+#             */
-/*   Updated: 2023/08/07 15:49:37 by mpuig-ma         ###   ########.fr       */
+/*   Updated: 2023/08/08 11:26:49 by framos-p         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
+
+static void	ft_swap(void **a, void **b);
 
 char	*ft_concat_list(t_list *list)
 {
@@ -42,6 +44,35 @@ char	*ft_concat_list(t_list *list)
 
 int	ft_sort_list(t_list *list)
 {
-	(void) list;
+	t_list	*current;
+	t_list	*min_node;
+	t_list	*temp;
+
+	current = list;
+	while (current != NULL)
+	{
+		min_node = current;
+		temp = current->next;
+		while (temp != NULL)
+		{
+			if (*((char *)temp->content) < *((char *)min_node->content))
+				min_node = temp;
+			temp = temp->next;
+		}
+		if (min_node != current)
+		{
+			ft_swap((void **)&current->content, (void **)&min_node->content);
+		}
+		current = current->next;
+	}
 	return (EXIT_SUCCESS);
+}
+
+static void	ft_swap(void **a, void **b)
+{
+	void	*temp;
+
+	temp = *a;
+	*a = *b;
+	*b = temp;
 }
