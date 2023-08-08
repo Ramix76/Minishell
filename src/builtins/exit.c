fab155964e1eb08663419a941c588c12ec3e205d
puyma <mpuig-ma@student.42barcelona.com>
2023-08-08 15:52:18 +0200

    norm

--- src/builtins/exit.c
+++ src/builtins/exit.c
@@ -6,7 +6,7 @@
 /*   By: framos-p <framos-p@student.42barcel>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/08 11:36:55 by framos-p          #+#    #+#             */
-/*   Updated: 2023/08/08 12:56:59 by mpuig-ma         ###   ########.fr       */
+/*   Updated: 2023/08/08 14:59:36 by mpuig-ma         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -29,7 +29,8 @@ int	ft_exit(t_cmd *cmd, t_data *data)
 	if (len > 1 && ft_str_isdigit(cmd->tokens[1]) == EXIT_FAILURE)
 	{
 		data->exit_code = 2;
-		ft_fprintf(stderr, "%s: exit: %s: numeric argument required\n", SH_NAME, cmd->tokens[1]);
+		ft_fprintf(stderr, "%s: exit: %s: numeric argument required\n",
+			SH_NAME, cmd->tokens[1]);
 	}
 	else if (len > 1)
 		data->exit_code = ft_atoi(cmd->tokens[1]);
