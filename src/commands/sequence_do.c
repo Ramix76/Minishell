b5f693b841cc0b7c23d18b4a53eced9629f559b4
puyma <mpuig-ma@student.42barcelona.com>
2023-08-09 11:04:17 +0200

    mv to simple_command

--- src/commands/sequence_do.c
+++ src/commands/sequence_do.c
@@ -6,7 +6,7 @@
 /*   By: mpuig-ma <mpuig-ma@student.42barcel>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/08 18:20:28 by mpuig-ma          #+#    #+#             */
-/*   Updated: 2023/08/08 18:36:10 by mpuig-ma         ###   ########.fr       */
+/*   Updated: 2023/08/09 10:41:46 by mpuig-ma         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,25 +18,25 @@ int	ft_sequence_do(char **tokens, int start, int end, t_data *data)
 {
 	size_t	arr_len;
 	char	**job;
+	int		ret;
 
+	ret = EXIT_SUCCESS;
 	arr_len = end - start + 1;
 	job = ft_arrndup(tokens + start, arr_len);
 	if (job == NULL)
 		return (errno = ENOMEM, EXIT_FAILURE);
 	if (ft_has_pipe(tokens) == 0)
-	{
-		if (ft_redirections_do(job, data) == EXIT_FAILURE
-			|| ft_redirections_rm(job) == EXIT_FAILURE
-			|| ft_simple_command(job, data) == EXIT_FAILURE)
-			return (ft_free_arr(job), EXIT_FAILURE);
-	}
+		ret = ft_simple_command_do(job, data);
 	else
 	{
 		if (ft_pipeline(tokens, data) == EXIT_FAILURE)
+		{
 			ft_fprintf(stderr, "ft_pipeline return FAILURE\n");
+			ret = EXIT_FAILURE;
+		}
 	}
 	ft_reset_redirections(data);
-	return (ft_free_arr(job), EXIT_SUCCESS);
+	return (ft_free_arr(job), ret);
 }
 
 static int	ft_has_pipe(char **tokens)
