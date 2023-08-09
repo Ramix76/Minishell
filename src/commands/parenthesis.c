f66b5cc51d32ae7c35b9c5748151e176f9f07915
puyma <mpuig-ma@student.42barcelona.com>
2023-08-09 16:35:49 +0200

    rm printfs

--- src/commands/parenthesis.c
+++ src/commands/parenthesis.c
@@ -6,7 +6,7 @@
 /*   By: mpuig-ma <mpuig-ma@student.42barcel>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/09 13:22:16 by mpuig-ma          #+#    #+#             */
-/*   Updated: 2023/08/09 13:31:48 by mpuig-ma         ###   ########.fr       */
+/*   Updated: 2023/08/09 15:12:51 by mpuig-ma         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,10 +14,13 @@
 
 int	ft_parenthesis_do(char **tokens, int start, int end, t_data *data)
 {
-	printf("()\n");
-	(void) tokens;
-	(void) start;
-	(void) end;
-	(void) data;
+	size_t	arr_len;
+	char	**job;
+
+	arr_len = end - start + 1;
+	job = ft_arrndup(tokens + start, arr_len);
+	if (job == NULL)
+		return (errno = ENOMEM, EXIT_FAILURE);	
+	ft_command_do(job, data);
 	return (EXIT_SUCCESS);
 }
