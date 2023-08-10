1d3cf24b0d6f2212db14435661cfb6279bb3a70a
puyma <mpuig-ma@student.42barcelona.com>
2023-08-10 11:35:04 +0200

    norm

--- src/parse/syntax_parenthesis.c
+++ src/parse/syntax_parenthesis.c
@@ -6,7 +6,7 @@
 /*   By: mpuig-ma <mpuig-ma@student.42barcel>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/09 17:41:43 by mpuig-ma          #+#    #+#             */
-/*   Updated: 2023/08/09 17:54:38 by mpuig-ma         ###   ########.fr       */
+/*   Updated: 2023/08/10 11:33:22 by mpuig-ma         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,7 +25,7 @@ int	ft_syntax_parenthesis(char **tokens, int i)
 		if (ft_strchr(METACHARACTERS, *(tokens[i - 1])) != 0)
 		{
 			ft_fprintf(stderr, "%s: syntax error near unexpected token `)'\n",
-			SH_NAME);
+				SH_NAME);
 			return (EXIT_FAILURE);
 		}
 	}
