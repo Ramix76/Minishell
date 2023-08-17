38468cc63ff8cbedbb564b09baf58bf7ce5f2811
puyma <mpuig-ma@student.42barcelona.com>
2023-08-17 13:54:13 +0200

    redo: ft_expand_dollar

--- src/expansions/parameter.c
+++ src/expansions/parameter.c
@@ -12,21 +12,71 @@
 
 #include "minishell.h"
 
-static char	*ft_replace_dollar(char *expanded, char *dollar, t_data *data);
-static char	*ft_getname(char *ptr);
-static char	*ft_getvalue(char *ptr, t_data *data);
+char			*ft_expand_dollar(char *str, t_data *data);
+static size_t	ft_expand_dollar_len(char *str, t_data *data);
+static char		*ft_getname(char *ptr);
+static char		*ft_getvalue(char *ptr, t_data *data);
 
-char	*ft_expand_dollar(char *str, t_data *data)
+char	*ft_expand_dollar(char *ptr, t_data *data)
+{
+	char	*expanded;
+	size_t	i;
+	size_t	len;
+	int		quote;
+	char	*name;
+	char	*value;
+
+	i = 0;
+	len = ft_expand_dollar_len(ptr, data);
+	quote = '\0';
+	expanded = (char *) ft_calloc(sizeof(char), len + 1);
+	while (i < len)
+	{
+		if (quote == '\0' && (*ptr == 042 || *ptr == 047))
+		{
+			expanded[i] = *ptr;
+			quote = *ptr;
+		}
+		else if (quote != '\0' && quote == *ptr)
+		{
+			expanded[i] = *ptr;
+			quote = '\0';
+		}
+		else if ((quote == '\0' || quote == 042) && *ptr == '$')
+		{
+			name = ft_getname(ptr);
+			if (name != NULL)
+			{
+				value = ft_getvalue(name, data);
+				if (value != NULL)
+				{
+					ft_strlcat(expanded, value, len + 1);
+					i += ft_strlen(value);
+					--i;
+				}
+				ptr = ptr + ft_strlen(name);
+			}
+			else
+				expanded[i] = *ptr;
+		}
+		else
+			expanded[i] = *ptr;
+		++i;
+		++ptr;
+	}
+	return (expanded);
+}
+
+static size_t	ft_expand_dollar_len(char *ptr, t_data *data)
 {
 	int		quote;
-	char	*ptr;
-	char	*temp;
-	char	*expanded;
+	size_t	len;
+	char	*name;
+	char	*value;
 
 	quote = '\0';
-	expanded = ft_strdup(str);
-	ptr = expanded;
-	while (ptr != NULL && *ptr != '\0')
+	len = 0;
+	while (*ptr != '\0')
 	{
 		if (quote == '\0' && (*ptr == 042 || *ptr == 047))
 			quote = *ptr;
@@ -34,41 +84,20 @@ char	*ft_expand_dollar(char *str, t_data *data)
 			quote = '\0';
 		else if ((quote == '\0' || quote == 042) && *ptr == '$')
 		{
-			temp = expanded;
-			expanded = ft_replace_dollar(expanded, ptr, data);
-			ptr = expanded;
-			(void) temp;
+			name = ft_getname(ptr);
+			if (name != NULL)
+			{
+				--len;
+				value = ft_getvalue(name, data);
+				if (value != NULL)
+					len += ft_strlen(value);
+				ptr = ptr + ft_strlen(name);
+			}
 		}
+		++len;
 		++ptr;
 	}
-	return (expanded);
-}
-
-static char	*ft_replace_dollar(char *expanded, char *dollar, t_data *data)
-{
-	char	*name;
-	char	*value;
-	char	*temp;
-	size_t	name_len;
-	size_t	p;
-
-	if (*(dollar + 1) == '?')
-		name = ft_strdup("?");
-	else
-		name = ft_getname(dollar);
-	if (name != NULL)
-	{
-		name_len = ft_strlen(name);
-		value = ft_getvalue(name, data);
-		ft_memmove(dollar + 1, dollar + 1 + name_len,
-			ft_strlen(dollar + name_len));
-		p = dollar - expanded;
-		temp = expanded;
-		expanded = ft_strpjoin_replace(expanded, value, p);
-		free(temp);
-		free(name);
-	}
-	return (expanded);
+	return (len);
 }
 
 /*
@@ -92,7 +121,9 @@ static char	*ft_getname(char *ptr)
 		return (NULL);
 	++ptr;
 	name_len = 0;
-	if (*ptr == '\0' && (ft_isalpha(*ptr) == 0 || *ptr != '_'))
+	if (*ptr == '?')
+		return (ft_strdup("?"));
+	else if (*ptr == '\0' || (ft_isalpha(*ptr) == 0 && *ptr != '_'))
 		return (NULL);
 	while (ptr[name_len] != '\0' && (ft_isalnum(ptr[name_len]) != 0
 			|| ptr[name_len] == '_'))
