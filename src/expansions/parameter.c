c24b48014805a0eda7d3ea1094821e8e428116c9
puyma <mpuig-ma@student.42barcelona.com>
2023-08-22 13:07:38 +0200

    end norm

--- src/expansions/parameter.c
+++ src/expansions/parameter.c
@@ -6,7 +6,7 @@
 /*   By: mpuig-ma <mpuig-ma@student.42barcel>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/11 16:32:15 by mpuig-ma          #+#    #+#             */
-/*   Updated: 2023/08/22 09:46:31 by mpuig-ma         ###   ########.fr       */
+/*   Updated: 2023/08/22 13:07:19 by mpuig-ma         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,35 +15,30 @@
 char			*ft_expand_dollar(char *str, t_data *data);
 static size_t	ft_expand_dollar_len(char *str, t_data *data);
 static size_t	ft_resolve_len(char *ptr, t_data *data, size_t *len_ptr);
-static int		ft_expand_dollar_value(char *expanded, char *name,
+static int		ft_expand_dollar_value(char *expanded,
 					size_t len, t_data *data);
+static int		ft_modified_quote(int *quote_ptr, char *ptr);
 
 char	*ft_expand_dollar(char *ptr, t_data *data)
 {
-	char	*expanded;
-	size_t	i;
-	size_t	len;
-	int		quote;
-	char	*name;
+	char			*expanded;
+	size_t			i;
+	const size_t	len = ft_expand_dollar_len(ptr, data);
+	int				quote;
 
 	i = 0;
-	len = ft_expand_dollar_len(ptr, data);
 	quote = '\0';
 	expanded = (char *) ft_calloc(sizeof(char), len + 1);
 	while (i < len)
 	{
-		if (quote == '\0' && (*ptr == 042 || *ptr == 047))
-			quote = *ptr;
-		else if (quote != '\0' && quote == *ptr)
-			quote = '\0';
-		else if ((quote == '\0' || quote == 042) && *ptr == '$')
+		if (ft_modified_quote(&quote, ptr) == 0
+			&& (quote == '\0' || quote == 042) && *ptr == '$')
 		{
-			name = ft_getname(ptr);
-			if (name != NULL)
+			data->temp = ft_getname(ptr);
+			if (data->temp != NULL)
 			{
-				ptr = ptr + ft_strlen(name) + 1;
-				i += ft_expand_dollar_value(expanded, name, len, data);
-				free(name);
+				ptr = ptr + ft_strlen(data->temp) + 1;
+				i += ft_expand_dollar_value(expanded, len, data);
 				continue ;
 			}
 		}
@@ -52,13 +47,36 @@ char	*ft_expand_dollar(char *ptr, t_data *data)
 	return (expanded);
 }
 
-static int	ft_expand_dollar_value(char *expanded, char *name,
-		size_t len, t_data *data)
+static int	ft_modified_quote(int *quote_ptr, char *ptr)
 {
+	int	modified;
+	int	quote;
+
+	modified = 0;
+	quote = *quote_ptr;
+	if (quote == '\0' && (*ptr == 042 || *ptr == 047))
+	{
+		quote = *ptr;
+		modified = 1;
+	}
+	else if (quote != '\0' && quote == *ptr)
+	{
+		quote = '\0';
+		modified = 1;
+	}
+	if (modified == 1)
+		*quote_ptr = quote;
+	return (modified);
+}
+
+static int	ft_expand_dollar_value(char *expanded, size_t len, t_data *data)
+{
+	char	*name;
 	char	*value;
 	size_t	i;
 
 	i = 0;
+	name = data->temp;
 	value = ft_getvalue(name, data);
 	if (value != NULL)
 	{
@@ -66,6 +84,7 @@ static int	ft_expand_dollar_value(char *expanded, char *name,
 		i += ft_strlen(value);
 		--i;
 	}
+	free(data->temp);
 	++i;
 	return (i);
 }
